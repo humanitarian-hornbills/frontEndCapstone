7f9d9404780594072df26500bc625ca73c498d5b
gusfel <agustinjfeliciano@gmail.com>
2021-02-24 21:41:27 -0800

    trying to fix

---
+++ config.js
@@ -0,0 +1,6 @@
+const api = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
+const TOKEN = '4f6a814564be855dab19d5a450611409c59e87a8';
+
+module.exports = {
+  api, TOKEN,
+};
---
+++ src/components/QA.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QA = () => (
+  <div>Hi</div>
+);
+
+export default QA;
