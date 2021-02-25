7829732506c1037c776864f451fc1f582e2bcda9
jincheng411 <jincheng411@gmail.com>
2021-02-25 00:17:02 -0500

    add related products component

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -1,1 +1,11 @@
-const React = require('react';)
+const React = require('react');
+
+class RelatedProducts extends React.Component {
+  render() {
+    return (
+      <div>Hi from Jin</div>
+    )
+  }
+};
+
+export default RelatedProducts;
